12c2932290e02dc2b7656040ceaa2824c231ff97
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 07:27:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  new_array = new.Array
+end
+
+def array_with_two_elements
+  new_array = [1, 2]
+end
+
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_wtih_array_method
+  
+end
